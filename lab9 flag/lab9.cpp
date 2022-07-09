f4748847adaa66e83063f900d42f8b8021d366b3
Thiloth b shetty <93128235+thiloth@users.noreply.github.com>
2022-07-09 12:45:19 +0530

    Update lab9.cpp

--- lab9 flag/lab9.cpp
+++ lab9 flag/lab9.cpp
@@ -2,29 +2,33 @@
 #include<stdio.h>
 #include<math.h>
 #define PI 3.1416
+
 GLsizei winWidth = 600, winHeight = 600;
 GLfloat xwcMin = 0.0, xwcMax = 130.0;
 GLfloat ywcMin = 0.0, ywcMax = 130.0;
+int flag;
 struct wcPt3D
 {
-	GLfloat x, y, z;
+GLfloat x, y, z;
 };
+
+
 void bino(GLint n, GLint *C)
 {
 	GLint k, j;
 	for(k=0;k<=n;k++)
 	{
 		C[k]=1;
-		for(j=n;j>=k+1;j--)
+	for(j=n;j>=k+1;j--)
 		C[k]*=j;
-		for(j=n-k;j>=2;j--)
+	for(j=n-k;j>=2;j--)
 		C[k]/=j;
 	}
 }
 
+
 void computeBezPt(GLfloat u,struct wcPt3D *bezPt, GLint nCtrlPts, struct wcPt3D *ctrlPts, GLint *C)
 {
-
 	GLint k, n=nCtrlPts-1;
 	GLfloat bezBlendFcn;
 	bezPt ->x =bezPt ->y = bezPt->z=0.0;
@@ -36,7 +40,9 @@ void computeBezPt(GLfloat u,struct wcPt3D *bezPt, GLint nCtrlPts, struct wcPt3D 
 		bezPt ->z += ctrlPts[k].z * bezBlendFcn;
 	}
 }
-void bezier(struct wcPt3D *ctrlPts, GLint nCtrlPts, GLint nBezCurvePts)
+
+
+void bezier(struct wcPt3D *ctrlPts, GLint nCtrlPts, GLint nBezCurvePts)	
 {
 	struct wcPt3D bezCurvePt;
 	GLfloat u;
@@ -45,18 +51,28 @@ void bezier(struct wcPt3D *ctrlPts, GLint nCtrlPts, GLint nBezCurvePts)
 	glBegin(GL_LINE_STRIP);
 	for(k=0;k<=nBezCurvePts;k++)
 	{
-		u= GLfloat(k)/GLfloat(nBezCurvePts);
+		u=GLfloat(k)/GLfloat(nBezCurvePts);
 		computeBezPt(u,&bezCurvePt, nCtrlPts, ctrlPts, C);
 		glVertex2f(bezCurvePt.x,bezCurvePt.y);
 	}
 	glEnd();
 
 }
+
+
 void displayFcn()
 {
 	GLint nCtrlPts = 4, nBezCurvePts =20;
 	static float theta = 0;
 	struct wcPt3D ctrlPts[4] = {{20, 100, 0},{30, 110, 0},{50, 90, 0},{60, 100, 0}};
+       if (flag==0)
+        {
+        glClear(GL_COLOR_BUFFER_BIT);
+        //glFlush();
+       } 
+     else
+       {
+
 	ctrlPts[1].x +=10*sin(theta * PI/180.0);
 	ctrlPts[1].y +=5*sin(theta * PI/180.0);
 	ctrlPts[2].x -= 10*sin((theta+30) * PI/180.0);
@@ -78,13 +94,12 @@ void displayFcn()
 	glColor3f(1, 1, 1); 
 	for(int i=0;i<8;i++)
 	{
-
 		glTranslatef(0, -0.8, 0);
 		bezier(ctrlPts,nCtrlPts, nBezCurvePts);
 	}
 	glColor3f(19/255.0, 136/255.0, 8/255.0); 
 	for(int i=0;i<8;i++)
-	{
+	{	
 		glTranslatef(0,-0.8, 0);
 		bezier(ctrlPts, nCtrlPts, nBezCurvePts);
 	}
@@ -95,13 +110,23 @@ void displayFcn()
 	glVertex2f(20,100);
 	glVertex2f(20,40);
 	glEnd();
-	glFlush();
-	glutPostRedisplay();
+
+      
+       }
+  glFlush();
+    
 	glutSwapBuffers();
+glutPostRedisplay();
+	
+	}
+
+
+void disp()
+{
+	displayFcn();
 }
-void disp(){
-	glClear(GL_COLOR_BUFFER_BIT);
-}
+
+
 void winReshapeFun(GLint newWidth, GLint newHeight)
 {
 	glViewport(0, 0, newWidth, newHeight);
@@ -110,21 +135,27 @@ void winReshapeFun(GLint newWidth, GLint newHeight)
 	gluOrtho2D(xwcMin, xwcMax, ywcMin, ywcMax);
 }
 
+
 void menu(int c){
 	switch(c){
-		case 1:glutDisplayFunc(displayFcn);
-			displayFcn();
+		case 1://glutDisplayFunc(displayFcn);
+			//displayFcn();
+                        flag=1;
+                      // glutPostRedisplay();  
 			break;
 		case 2:exit(0);
 	}
 }
+
+
+
 int main(int argc, char **argv)
 {
 	glutInit(& argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowPosition(50, 50);
 	glutInitWindowSize(winWidth, winHeight);
-	glutCreateWindow("4MT19CS171 Bezier Curve");
+	glutCreateWindow("Bezier Curve--->4MT19CS134");
 	glutDisplayFunc(disp);
 	glutReshapeFunc(winReshapeFun);
 	glutCreateMenu(menu);
